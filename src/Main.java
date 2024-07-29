884bf762194c3b4cb830ed452d2c7bfb50aa5ae8
jonathanspereira <jonathanpereira.jsp@outlook.com>
2024-07-28 21:27:46 -0300

    criando banco com poo

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Cliente joao = new Cliente();
+        joao.setNome("Joao");
+
+        Conta cc = new ContaCorrente(joao);
+        Conta poupanca = new ContaPoupanca(joao);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
+}
